773146d69a9390403f10fe14fc9a6ac2904c53cd
Kemal Toygun Yıldız <toyguny@hotmail.com>
2025-05-20 13:35:05 +0300

    Enhance project structure with Docker and EF Core support

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,23 +1,53 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
-namespace Catalog
+namespace Catalog;
+public static class CatalogModule
 {
-    public static class CatalogModule
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+        // Add services to the container.
+
+        // Api Endpoint services
+
+        // Application Use Case services
+        services.AddMediatR(config =>
         {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
 
+        // Data - Infrastructure services
+        var connectionString = configuration.GetConnectionString("Database");
 
-            return services;
-        }
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
-        public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+        services.AddDbContext<CatalogDbContext>((sp, options) =>
         {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
 
+        services.AddScoped<IDataSeeder, CatalogDataSeeder>();
 
-            return app;
-        }
+        return services;
+    }
+
+    public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+
+        // 1. Use Api Endpoint services
+
+        // 2. Use Application Use Case services
+
+        // 3. Use Data - Infrastructure services  
+        app.UseMigration<CatalogDbContext>();
+
+        return app;
     }
 }
