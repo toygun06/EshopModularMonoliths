9ed7132036b9d7f606baca2d5f2270490088ef11
Kemal Toygun Yıldız <toyguny@hotmail.com>
2025-05-25 14:26:57 +0300

    Added Seq

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,23 +1,53 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿//using Basket.Data.Processors;
+//using Basket.Data.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
+using Shared.Data.Interceptors;
 
-namespace Basket
+namespace Basket;
+public static class BasketModule
 {
-    public static class BasketModule
+    public static IServiceCollection AddBasketModule(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration)
-        {
+        // Add services to the container.
+        // 1. Api Endpoint services
 
+        // 2. Application Use Case services
+        //services.AddScoped<IBasketRepository, BasketRepository>();
+        //services.Decorate<IBasketRepository, CachedBasketRepository>();
 
-            return services;
-        }
+        // 3. Data - Infrastructure services
+        //var connectionString = configuration.GetConnectionString("Database");
 
-        public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
-        {
+        //services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        //services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
+        //services.AddDbContext<BasketDbContext>((sp, options) =>
+        //{
+        //    options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+        //    options.UseNpgsql(connectionString);
+        //});
 
-            return app;
-        }
+        //services.AddHostedService<OutboxProcessor>();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+        // 1. Use Api Endpoint services
+
+        // 2. Use Application Use Case services
+
+        // 3. Use Data - Infrastructure services
+        //app.UseMigration<BasketDbContext>();
+
+        return app;
     }
 }
